2bb7c78678331c52316c5052b384f17c7a176548
cluisAA <cliveluis@outlook.com>
2025-11-27 04:24:00 +0000

    Create 1. create_raw_table.sql

---
+++ 1. create_raw_table.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE TABLE APPLE_MUSIC_DB.RAW.apple_music_play_activity_raw (
+    -- Paste Python-generated columns here
+    -- Example:
+    -- "Age Bucket" VARCHAR,
+    -- "Album Name" VARCHAR,
+    -- ...
+);
